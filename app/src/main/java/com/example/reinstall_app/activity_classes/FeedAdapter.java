61f264aeea442c40e0576a86b6109963f2d72be2
pinmistro <deviousdesigns16@gmail.com>
2020-12-30 13:06:59 +0200

    feed changes

---
+++ app/src/main/java/com/example/reinstall_app/activity_classes/FeedAdapter.java
@@ -0,0 +1,24 @@
+package com.example.reinstall_app.activity_classes;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FeedAdapter extends RecyclerView.Adapter {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
