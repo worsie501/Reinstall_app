2839b0b1824c2b43205cde48094ad6411b6bc322
pinmistro <deviousdesigns16@gmail.com>
2021-02-18 06:50:44 +0200

    Category check when added

--- app/src/main/java/com/example/reinstall_app/activity_classes/CategoryFragment.java
+++ app/src/main/java/com/example/reinstall_app/activity_classes/CategoryFragment.java
@@ -15,8 +15,11 @@ import androidx.fragment.app.Fragment;
 import com.backendless.Backendless;
 import com.backendless.async.callback.AsyncCallback;
 import com.backendless.exceptions.BackendlessFault;
+import com.backendless.persistence.DataQueryBuilder;
 import com.example.reinstall_app.R;
 import com.example.reinstall_app.app_data.ProblemType;
+
+import java.util.List;
 
 public class CategoryFragment extends Fragment
 {
@@ -47,21 +50,78 @@ public class CategoryFragment extends Fragment
             @Override
             public void onClick(View v) {
 
-                ProblemType problemType = new ProblemType();
-                problemType.setProblemName(etNewCategory.getText().toString().trim());
-                problemType.setProblemDescription(etNewDesc.getText().toString().trim());
 
-                Backendless.Persistence.save(problemType, new AsyncCallback<ProblemType>() {
+              DataQueryBuilder queryBuilder = DataQueryBuilder.create();
+              queryBuilder.setPageSize(80);
+
+
+                Backendless.Persistence.of(ProblemType.class).find(queryBuilder, new AsyncCallback<List<ProblemType>>() {
                     @Override
-                    public void handleResponse(ProblemType response) {
-                        Toast.makeText(getActivity(), "Category successfully added!", Toast.LENGTH_SHORT).show();
+                    public void handleResponse(List<ProblemType> response) {
+
+                        boolean bFound = false;
+
+                        if(etNewCategory.getText().toString().isEmpty() | etNewDesc.getText().toString().isEmpty())
+                        {
+                            Toast.makeText(getContext(), "Please enter all fields", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+
+                            ProblemType problemType = new ProblemType();
+                            problemType.setProblemName(etNewCategory.getText().toString().trim());
+                            problemType.setProblemDescription(etNewDesc.getText().toString().trim());
+
+                            for(int i = 0; i < response.size(); i++)
+                            {
+
+                                if (etNewCategory.getText().toString().trim().equals(response.get(i).getProblemName()))
+                                {
+
+                                    bFound = true;
+                                    i = response.size();
+                                    Toast.makeText(getContext(), "Category already exists", Toast.LENGTH_SHORT).show();
+
+                                }
+                                else
+                                {
+                                    bFound = false;
+                                }
+
+                            }
+
+                            if(bFound == false)
+                            {
+                                Backendless.Persistence.save(problemType, new AsyncCallback<ProblemType>() {
+                                    @Override
+                                    public void handleResponse(ProblemType response) {
+
+                                        etNewCategory.setText("");
+                                        etNewDesc.setText("");
+                                        Toast.makeText(getActivity(), "Category successfully added!", Toast.LENGTH_SHORT).show();
+                                    }
+
+                                    @Override
+                                    public void handleFault(BackendlessFault fault) {
+                                        Toast.makeText(getActivity(), "Error: " + fault.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+                            }
+
+
+                        }
+
                     }
 
                     @Override
                     public void handleFault(BackendlessFault fault) {
-                        Toast.makeText(getActivity(), "Error: " + fault.getMessage(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Error: " + fault.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
+
+
+
 
             }
         });
